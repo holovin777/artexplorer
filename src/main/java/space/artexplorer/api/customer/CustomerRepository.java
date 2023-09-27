ed282b835f55626f5af517b7ef512bb5da24672f
Viktor Holovin <holovin@mail.com>
2023-09-27 12:39:43 +0200

    Add customer test

--- src/main/java/space/artexplorer/api/customer/CustomerRepository.java
+++ src/main/java/space/artexplorer/api/customer/CustomerRepository.java
@@ -1,8 +1,20 @@
 package space.artexplorer.api.customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    @Query(
+            value = "select id, first_name, last_name, phone_number " +
+                    "from customer where phone_number = :phone_number",
+            nativeQuery = true
+    )
+    Optional<Customer> selectCustomerByPhoneNumber(
+            @Param("phone_number") String phoneNumber
+    );
 }
